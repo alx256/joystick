4cf237676fd079815e5947e18b8de98263350c00
alxx0 <alexanderipoulson@gmail.com>
2020-02-12 15:18:43 +0000

    Added license to source files

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#  Copyright 2020 Alexander Poulson
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 J_BOOST_LINK=/usr/local/include/
 J_ARGS="-pthread -std=c++17 -I/usr/local/include"
 J_FAILED=false
